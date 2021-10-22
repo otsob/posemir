dd3f2730d8b8a1ac35ce2e2bd0ac5714e0eea688
Otso Björklund <otso.bjorklund@gmail.com>
2021-10-22 10:34:22 +0300

    Add type for points with integer components

--- point_set_algorithms/src/point_set/point.rs
+++ point_set_algorithms/src/point_set/point.rs
@@ -45,7 +45,7 @@ impl Point for Point2dF {
 
 // Traits for by value arithmetic
 impl ops::Add<Point2dF> for Point2dF {
-    type Output = Point2dF;
+    type Output = Self;
 
     fn add(self, rhs: Point2dF) -> Point2dF {
         Point2dF { x: self.x + rhs.x, y: self.y + rhs.y }
@@ -53,7 +53,7 @@ impl ops::Add<Point2dF> for Point2dF {
 }
 
 impl ops::Sub<Point2dF> for Point2dF {
-    type Output = Point2dF;
+    type Output = Self;
 
     fn sub(self, rhs: Point2dF) -> Self::Output {
         Point2dF { x: self.x - rhs.x, y: self.y - rhs.y }
@@ -61,7 +61,7 @@ impl ops::Sub<Point2dF> for Point2dF {
 }
 
 impl ops::Mul<f64> for Point2dF {
-    type Output = Point2dF;
+    type Output = Self;
 
     fn mul(self, rhs: f64) -> Self::Output {
         Point2dF { x: self.x * rhs, y: self.y * rhs }
@@ -136,6 +136,118 @@ impl Ord for Point2dF {
     }
 }
 
+
+/// Represents a 2-dimensional point/vector with integer components.
+#[derive(Debug, Copy)]
+pub struct Point2dI {
+    /// The x coordinate of the point
+    pub x: i64,
+    /// The y coordinate of the point
+    pub y: i64,
+}
+
+impl Point for Point2dI {
+    /// Returns true if this point is zero.
+    fn is_zero(&self) -> bool {
+        self.x == 0 && self.y == 0
+    }
+}
+
+// Traits for by value arithmetic
+impl ops::Add<Point2dI> for Point2dI {
+    type Output = Self;
+
+    fn add(self, rhs: Point2dI) -> Point2dI {
+        Point2dI { x: self.x + rhs.x, y: self.y + rhs.y }
+    }
+}
+
+impl ops::Sub<Point2dI> for Point2dI {
+    type Output = Self;
+
+    fn sub(self, rhs: Point2dI) -> Self::Output {
+        Point2dI { x: self.x - rhs.x, y: self.y - rhs.y }
+    }
+}
+
+impl ops::Mul<f64> for Point2dI {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        let rhs_int = rhs as i64;
+        Point2dI { x: self.x * rhs_int, y: self.y * rhs_int }
+    }
+}
+
+// Traits for by reference arithmetic
+impl ops::Add<&Point2dI> for &Point2dI {
+    type Output = Point2dI;
+
+    fn add(self, rhs: &Point2dI) -> Point2dI {
+        Point2dI { x: self.x + rhs.x, y: self.y + rhs.y }
+    }
+}
+
+impl ops::Sub<&Point2dI> for &Point2dI {
+    type Output = Point2dI;
+
+    fn sub(self, rhs: &Point2dI) -> Self::Output {
+        Point2dI { x: self.x - rhs.x, y: self.y - rhs.y }
+    }
+}
+
+impl ops::Mul<f64> for &Point2dI {
+    type Output = Point2dI;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        let rhs_int = rhs as i64;
+        Point2dI { x: self.x * rhs_int, y: self.y * rhs_int }
+    }
+}
+
+// Comparisons
+impl PartialEq for Point2dI {
+    fn eq(&self, other: &Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+impl Clone for Point2dI {
+    fn clone(&self) -> Self {
+        Point2dI { x: self.x, y: self.y }
+    }
+}
+
+impl Eq for Point2dI {}
+
+impl PartialOrd for Point2dI {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for Point2dI {
+    fn cmp(&self, other: &Self) -> Ordering {
+        if self.x < other.x {
+            return Ordering::Less;
+        }
+
+        if self.x > other.x {
+            return Ordering::Greater;
+        }
+
+        if self.y < other.y {
+            return Ordering::Less;
+        }
+
+        if self.y > other.y {
+            return Ordering::Greater;
+        }
+
+        Ordering::Equal
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -144,6 +256,7 @@ mod tests {
     fn test_eq() {
         assert_eq!(Point2dF { x: 3.0, y: 1.0 }, Point2dF { x: 3.0, y: 1.0 });
         assert_ne!(Point2dF { x: 3.0, y: 1.0 }, Point2dF { x: 3.0, y: 2.0 });
+        assert_ne!(Point2dI { x: 3, y: 1 }, Point2dI { x: 3, y: 2 });
     }
 
     #[test]
@@ -151,18 +264,38 @@ mod tests {
         let a = Point2dF { x: 1.0, y: 1.0 };
         let b = Point2dF { x: 2.0, y: 0.0 };
         assert_eq!(Point2dF { x: 3.0, y: 1.0 }, a + b);
+
+        let a = Point2dI { x: 1, y: 1 };
+        let b = Point2dI { x: 2, y: 0 };
+        assert_eq!(Point2dI { x: 3, y: 1 }, a + b);
     }
 
     #[test]
     fn test_sub() {
         assert_eq!(Point2dF { x: -1.0, y: 1.0 }, Point2dF { x: 1.0, y: 2.0 } - Point2dF { x: 2.0, y: 1.0 });
+        assert_eq!(Point2dI { x: -1, y: 1 }, Point2dI { x: 1, y: 2 } - Point2dI { x: 2, y: 1 });
     }
 
     #[test]
-    fn test_cmp() {
+    fn test_cmp_floats() {
         let a = Point2dF { x: -1.0, y: 0.0 };
         let b = Point2dF { x: -0.5, y: 0.0 };
         let c = Point2dF { x: -0.5, y: 1.0 };
+
+        assert_eq!(Some(Ordering::Equal), a.partial_cmp(&a));
+        assert_eq!(Some(Ordering::Less), a.partial_cmp(&b));
+        assert_eq!(Some(Ordering::Greater), b.partial_cmp(&a));
+        assert!(a <= a);
+        assert!(a < b);
+        assert!(b < c);
+        assert!(c > a);
+    }
+
+    #[test]
+    fn test_cmp_ints() {
+        let a = Point2dI { x: -1, y: 0 };
+        let b = Point2dI { x: -0, y: 0 };
+        let c = Point2dI { x: -0, y: 1 };
 
         assert_eq!(Some(Ordering::Equal), a.partial_cmp(&a));
         assert_eq!(Some(Ordering::Less), a.partial_cmp(&b));
