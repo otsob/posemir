16e146989436b5685ac1c952a8e8d07fba5c55c5
Otso Björklund <otso.bjorklund@gmail.com>
2022-07-18 14:49:17 +0300

    Use r=1 in siar benchmarks

--- benchmark/benches/siar_benchmarks.rs
+++ benchmark/benches/siar_benchmarks.rs
@@ -10,22 +10,22 @@ use posemir_discovery::siar::SiaR;
 
 fn siar_benchmarks_with_random(c: &mut Criterion) {
     let config = data_loader::Config::default_counts(String::from("random/random_points_"));
-    let algorithm = SiaR { r: 3 };
-    mtp_benchmark::run_mtp_benchmarks(&algorithm, "SIAR(3)", &config, c);
+    let algorithm = SiaR { r: 1 };
+    mtp_benchmark::run_mtp_benchmarks(&algorithm, "SIAR(1)", &config, c);
 }
 
 fn siar_benchmarks_with_min_pattern_count(c: &mut Criterion) {
     let config =
         data_loader::Config::default_counts(String::from("min_pattern_count/min_pattern_count_"));
-    let algorithm = SiaR { r: 3 };
-    mtp_benchmark::run_mtp_benchmarks(&algorithm, "SIAR(3)", &config, c);
+    let algorithm = SiaR { r: 1 };
+    mtp_benchmark::run_mtp_benchmarks(&algorithm, "SIAR(1)", &config, c);
 }
 
 fn siar_benchmarks_with_max_pattern_count(c: &mut Criterion) {
     let config =
         data_loader::Config::default_counts(String::from("max_pattern_count/max_pattern_count_"));
-    let algorithm = SiaR { r: 3 };
-    mtp_benchmark::run_mtp_benchmarks(&algorithm, "SIAR(3)", &config, c);
+    let algorithm = SiaR { r: 1 };
+    mtp_benchmark::run_mtp_benchmarks(&algorithm, "SIAR(1)", &config, c);
 }
 
 criterion_group!(name = siar_benchmarks;
